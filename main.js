957316d5bc3120d9daca5da1a54402bf48965264
Zac Morton <zmorton8057@gmail.com>
2019-05-30 20:17:47 -0700

    Accessing Objects JS

---
+++ main.js
@@ -0,0 +1,14 @@
+var faveShows = [];
+for (var i = 0; i < 4; i++) {
+    var userInput = prompt('What is your # ' + (i + 1) + ' favorite show?');
+    faveShows.push(userInput)
+}
+faveShows.forEach(function (arrayElement) {
+    console.log(arrayElement.toLowerCase())
+}
+
+var arrayLogger = function(arr) {
+    for(var i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    } console.log('*********')
+} 
